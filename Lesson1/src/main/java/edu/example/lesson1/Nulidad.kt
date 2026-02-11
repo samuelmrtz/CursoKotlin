6ff53a66c52d7a70c885205d6fc795436b590a37
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-11 07:49:04 -0600

    Tercer Commit

---
+++ Lesson1/src/main/java/edu/example/lesson1/Nulidad.kt
@@ -0,0 +1,9 @@
+package edu.example.lesson1
+
+fun main(){
+    var fishFoodTreats = 6
+    if (fishFoodTreats != null) {
+        fishFoodTreats = fishFoodTreats.dec()
+    }
+    println(fishFoodTreats)
+}
