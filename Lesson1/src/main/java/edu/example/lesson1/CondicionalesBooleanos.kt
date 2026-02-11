09df080d1d2c72c1fc2b1b48b91d2198201bdfe6
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-11 07:57:56 -0600

    Cuarto Commit

--- Lesson1/src/main/java/edu/example/lesson1/CondicionalesBooleanos.kt
+++ Lesson1/src/main/java/edu/example/lesson1/CondicionalesBooleanos.kt
@@ -3,6 +3,8 @@ package edu.example.lesson1
 fun main(){
     val numberOfFish = 50
     val numberOfPlants = 23
+
+    //Uso de condicional If
     if (numberOfFish == 0) {
         println("Empty tank")
     } else if (numberOfFish < 40) {
@@ -14,7 +16,7 @@ fun main(){
     if (fish in 1..100) {
         println(fish)
     }
-
+//Condiconal When
     when (numberOfFish) {
         0  -> println("Empty tank")
         in 1..39 -> println("Got fish!")
--- Lesson1/src/main/java/edu/example/lesson1/VarVal.kt
+++ Lesson1/src/main/java/edu/example/lesson1/VarVal.kt
@@ -1,14 +1,17 @@
 package edu.example.lesson1
 fun main (){
+    //Variables Var y Val
     val i: Int = 6
     val b1 = i.toByte()
-    val b2: Byte = 1 // OK, literals are checked statically
+    val b2: Byte = 1
     println(b2)
     val i4: Int = b2.toInt()
     val i5: String = b2.toString()
     println(i5)
     val i6: Double = b2.toDouble()
     println(i6)
+
+    //Sintaxis que se puede usar con las variables
 
     val oneMillion = 1_000_000
     val socialSecurityNumber = 999_99_9999L
