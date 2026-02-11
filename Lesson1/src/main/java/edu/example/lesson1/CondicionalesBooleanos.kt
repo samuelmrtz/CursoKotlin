440f50c587250b97509f497738e6259003c4ac8b
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-11 07:38:37 -0600

    Segundo Commit

---
+++ Lesson1/src/main/java/edu/example/lesson1/CondicionalesBooleanos.kt
@@ -0,0 +1,23 @@
+package edu.example.lesson1
+
+fun main(){
+    val numberOfFish = 50
+    val numberOfPlants = 23
+    if (numberOfFish == 0) {
+        println("Empty tank")
+    } else if (numberOfFish < 40) {
+        println("Got fish!")
+    } else {
+        println("That's a lot of fish!")
+    }
+    val fish = 50
+    if (fish in 1..100) {
+        println(fish)
+    }
+
+    when (numberOfFish) {
+        0  -> println("Empty tank")
+        in 1..39 -> println("Got fish!")
+        else -> println("That's a lot of fish!")
+    }
+}
