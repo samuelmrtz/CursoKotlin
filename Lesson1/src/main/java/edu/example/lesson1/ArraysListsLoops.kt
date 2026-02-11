2c1599e6373a9aca0bcd75d38711991c430c8399
samuelmrtz <isaacmtzley01@gmail.com>
2026-02-11 08:04:30 -0600

    Quinto Commit

--- Lesson1/src/main/java/edu/example/lesson1/ArraysListsLoops.kt
+++ Lesson1/src/main/java/edu/example/lesson1/ArraysListsLoops.kt
@@ -1,16 +1,20 @@
 package edu.example.lesson1
 
 fun main(){
+
+    //Uso de arrays
     val school = arrayOf("shark", "salmon", "minnow")
     for (element in school) {
         print(element + " ")
     }
-    val myList = mutableListOf("tuna", "salmon", "shark")
-    myList.remove("shark")
 
     val school2 = arrayOf("shark", "salmon", "minnow")
     println(java.util.Arrays.toString(school2))
     val mix = arrayOf("fish", 2)
+
+    //Uso de listas
+    val myList = mutableListOf("tuna", "salmon", "shark")
+    myList.remove("shark")
 
     val numbers = intArrayOf(1, 2, 3)
     val oceans = listOf("Atlantic", "Pacific")
@@ -20,15 +24,19 @@ fun main(){
     val array = Array (5) { it * 2 }
     println(java.util.Arrays.toString(array))
 
+    //Uso de bucle for
+
     for ((index, element) in school.withIndex()) {
         println("Item at $index is $element\n")
     }
-
+    //Uso de bucle While
     var bubbles = 0
     while (bubbles < 50) {
         bubbles++
     }
     println("$bubbles bubbles in the water\n")
+
+    //Uso de bucle Do While
 
     do {
         bubbles--
--- Lesson1/src/main/java/edu/example/lesson1/Nulidad.kt
+++ Lesson1/src/main/java/edu/example/lesson1/Nulidad.kt
@@ -1,6 +1,7 @@
 package edu.example.lesson1
 
 fun main(){
+    //Uso de nulidad para realizar decremento de nulidad en una variable
     var fishFoodTreats = 6
     if (fishFoodTreats != null) {
         fishFoodTreats = fishFoodTreats.dec()
